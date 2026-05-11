56d8accb24169781fb85cf274f31ab0698a3c749
Zach Harel <zach@zharel.me>
2026-05-10 20:38:23 -0400

    start hardware content
    Signed-off-by: Zach Harel <zach@zharel.me>

---
+++ hardware/src/main/kotlin/dev/nextftc/hardware/RobotController.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 NextFTC Team
+ *
+ *  Use of this source code is governed by an BSD-3-clause
+ *  license that can be found in the LICENSE.md file at the root of this repository or at
+ *  https://opensource.org/license/bsd-3-clause.
+ */
+
+package dev.nextftc.hardware
+
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+
+object RobotController {
+    @JvmStatic
+    val hardwareMap get() = OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().activity).hardwareMap;
+}
