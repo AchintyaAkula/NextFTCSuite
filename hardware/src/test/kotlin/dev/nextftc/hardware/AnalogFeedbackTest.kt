b4a15ea113174216b61f48fe37c99fc5450c23b2
Zach Harel <zach@zharel.me>
2026-07-01 15:48:35 -0400

    update licensing
    Signed-off-by: Zach Harel <zach@zharel.me>

--- hardware/src/test/kotlin/dev/nextftc/hardware/AnalogFeedbackTest.kt
+++ hardware/src/test/kotlin/dev/nextftc/hardware/AnalogFeedbackTest.kt
@@ -1,19 +1,9 @@
 /*
- * NextFTC: a user-friendly control library for FIRST Tech Challenge
- *     Copyright (C) 2025 Rowan McAlpin
+ * Copyright (c) 2026 NextFTC Team
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  Use of this source code is governed by an BSD-3-clause
+ *  license that can be found in the LICENSE.md file at the root of this repository or at
+ *  https://opensource.org/license/bsd-3-clause.
  */
 
 package dev.nextftc.hardware
